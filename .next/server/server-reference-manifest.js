0945b4d6a3b5010ca472813aeaf2d0667a28358b
Varity MCP <varity-mcp@varity.so>
2026-04-27 01:17:52 -0400

    Build output for deployment

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+id5Kup8bIJ/wa8QvXOcib91RJrTyGqVxvsO8pLEHYA=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
